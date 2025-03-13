25eb193709ae33aea024cba29109d8403be947aa
Matthew Reichard <squid05@hotmail.com>
2025-03-13 16:58:43 -0500

    it worksss

--- hardwareloop.py
+++ hardwareloop.py
@@ -3,37 +3,37 @@ from wetness import read_moisture
 import datetime
 import json
 
-luxpin = 27 #pin for light relay
-
-h20pin = 17 #pin for pump relay
-
-ctime = datetime.datetime.now() #use ctime.hour, ctime.minute, ctime.second and so on
+luxpin = 27  # pin for light relay
+h20pin = 17  # pin for pump relay
 
 moisture = 0
-
 lighton = False
+last_squirt_time = None  # Store the last execution time
 
 while True:
     with open('/home/irl/ai-plant-table/static/data.json', 'r') as file:
         data = json.load(file)
-    
-    recmoist = data['moisture']
 
+    recmoist = data['moisture']
     reclux = data['sunlight']
 
     moisture = read_moisture()
     ctime = datetime.datetime.now()
 
-    if (ctime.minute%10) == 0:
-        if moisture < recmoist:
-            squirt(h20pin)
-        print("FYI, recmoist is "+str(recmoist)+" and reclux is "+str(reclux))
+    # Ensure squirt() runs only once per 10-minute interval
+    if (ctime.minute % 10) == 0:
+        if last_squirt_time is None or (ctime - last_squirt_time).total_seconds() >= 600:
+            if moisture < recmoist:
+                squirt(h20pin)
+                last_squirt_time = ctime  # Update the last execution time
+            print(f"FYI, recmoist is {recmoist} and reclux is {reclux}")
 
-    if (ctime.hour >= (12-(reclux/2))) and (ctime.hour < (12+(reclux/2))):
-        if lighton == False:
+    # Control the light based on sunlight data
+    if (ctime.hour >= (12 - (reclux / 2))) and (ctime.hour < (12 + (reclux / 2))):
+        if not lighton:
             lighton = True
             p_on(luxpin)
     else:
-        if lighton == True:
+        if lighton:
             lighton = False
             p_off(luxpin)

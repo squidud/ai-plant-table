fc97e0f8bfd06e42550a63843b8df02f3d2cc5f8
Matthew Reichard <squid05@hotmail.com>
2025-03-13 13:25:48 -0500

    added primary hardware loop

--- ezstart.sh
+++ ezstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ~/ai-plant-table/venv/bin/activate
 python ~/ai-plant-table/launcher.py &
+python ~/ai-plant-table/hardwareloop.py &
 sleep 5s
 chromium-browser --kiosk "http://127.0.0.1:5000"
 #nice
--- hardwareloop.py
+++ hardwareloop.py
@@ -1,8 +1,39 @@
 from clicky import p_off, p_on, squirt
-import time
+from wetness import read_moisture
 import datetime
+import json
 
 luxpin = 27 #pin for light relay
 
 h20pin = 17 #pin for pump relay
 
+ctime = datetime.datetime.now() #use ctime.hour, ctime.minute, ctime.second and so on
+
+moisture = 0
+
+lighton = False
+
+while True:
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    
+    recmoist = data['moisture']
+
+    reclux = data['sunlight']
+
+    moisture = read_moisture()
+    ctime = datetime.datetime.now()
+
+    if (ctime.minute%10) == 0:
+        if moisture < recmoist:
+            squirt(h20pin)
+        print("FYI, recmoist is "+str(recmoist)+" and reclux is "+str(reclux))
+
+    if (ctime.hour >= (12-(reclux/2))) and (ctime.hour < (12+(reclux/2))):
+        if lighton == False:
+            lighton = True
+            p_on(luxpin)
+    else:
+        if lighton == True:
+            lighton = False
+            p_off(luxpin)

7908b96ef789f4844d9e170a335082d07076853e
Matthew Reichard <squid05@hotmail.com>
2025-03-17 13:06:44 -0500

    fixed repeating moisture log entries

--- hardwareloop.py
+++ hardwareloop.py
@@ -25,17 +25,14 @@ while True:
     moisture = read_moisture()
     ctime = datetime.datetime.now()
 
-    if (ctime.minute % 30) == 0:
-        log_action("Moisture level: {moisture}%")
-
     # Ensure squirt() runs only once per 30-minute interval
     if (ctime.minute % 30) == 0:
         if last_squirt_time is None or (ctime - last_squirt_time).total_seconds() >= 600:
+            log_action("Moisture level: "+str(moisture)+"%")
             if moisture < recmoist:
                 log_action("Dispensed water.")
                 squirt(h20pin)
                 last_squirt_time = ctime  # Update the last execution time
-            print(f"FYI, recmoist is {recmoist} and reclux is {reclux}")
 
     # Control the light based on sunlight data
     if (ctime.hour >= (12 - (reclux / 2))) and (ctime.hour < (12 + (reclux / 2))):
